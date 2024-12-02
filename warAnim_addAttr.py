c1e4def56740719b90957f5ef5cdf54b8c215df6
asuei <lingasuei@gmail.com>
2024-12-02 23:32:24 +0800

    Create warAnim_addAttr.py

---
+++ warAnim_addAttr.py
@@ -0,0 +1,93 @@
+# encoding: utf-8
+import maya.cmds as cmds
+import re
+
+
+class as_warAttr :
+ def __init__(self):
+  if(cmds.window('win_warAttr',exists=1)):
+   cmds.deleteUI('win_warAttr')
+  cmds.window('win_warAttr',title='WarAnim addAttr')
+
+  cmds.formLayout('form_waaMain')
+  #cmds.button('btn_waaATB',label='Add Body Tab',height=30,command=self.addTag)
+  cmds.button('btn_waaATB',label='Add Body Tab',height=30,command=lambda *_: self.addTag('body'))
+  cmds.button('btn_waaATF',label='Add Face Tab',height=30,command=lambda *_: self.addTag('face'))
+  cmds.button('btn_waaATC',label='Add Tab (Input Below)',height=30,command=lambda *_: self.addTag('inputBelow'))
+  cmds.textField('txt_waaATC',text='',height=23)
+  cmds.separator('sprt1',style='in')
+  cmds.button('btn_waaPXY',label='Add Pos XY',height=30,command=self.addPosXY)
+  cmds.separator('sprt2',style='in')
+  cmds.button('btn_waaAAL',label='Add Attach List (Select objects)',height=30,command=self.addAttachObjs)
+  cmds.button('btn_waaAAS',label='Add Attach Side',height=30,command=self.addAttachSide)
+  cmds.separator('sprt3',style='in')
+  cmds.button('btn_waaASS',label='Add Shape Selection',height=30,command=self.addShape)
+  cmds.button('btn_waaAWH',label='Add Width Height',height=30,command=self.addWidthHeight)
+  cmds.separator('sprt4',style='in')
+
+  cmds.formLayout('form_waaMain',e=1,af=[('btn_waaATB','top',5),('btn_waaATB','left',5),('btn_waaATB','right',5)])
+  cmds.formLayout('form_waaMain',e=1,ac=('btn_waaATF','top',5,'btn_waaATB'),af=[('btn_waaATF','left',5),('btn_waaATF','right',5)])
+  cmds.formLayout('form_waaMain',e=1,ac=('btn_waaATC','top',5,'btn_waaATF'),af=[('btn_waaATC','left',5),('btn_waaATC','right',5)])
+  cmds.formLayout('form_waaMain',e=1,ac=('txt_waaATC','top',5,'btn_waaATC'),af=[('txt_waaATC','left',5),('txt_waaATC','right',5)])
+  cmds.formLayout('form_waaMain',e=1,af=[('sprt1','left',5),('sprt1','right',5)],ac=('sprt1','top',5,'txt_waaATC'))
+  cmds.formLayout('form_waaMain',e=1,af=[('btn_waaPXY','left',5),('btn_waaPXY','right',5)],ac=('btn_waaPXY','top',3,'sprt1'))
+  cmds.formLayout('form_waaMain',e=1,af=[('sprt2','left',5),('sprt2','right',5)],ac=('sprt2','top',5,'btn_waaPXY'))
+  cmds.formLayout('form_waaMain',e=1,af=[('btn_waaAAL','left',5),('btn_waaAAL','right',5)],ac=('btn_waaAAL','top',5,'sprt2'))
+  cmds.formLayout('form_waaMain',e=1,af=[('btn_waaAAS','left',5),('btn_waaAAS','right',5)],ac=('btn_waaAAS','top',3,'btn_waaAAL'))
+  cmds.formLayout('form_waaMain',e=1,af=[('sprt3','left',5),('sprt3','right',5)],ac=('sprt3','top',5,'btn_waaAAS'))
+  cmds.formLayout('form_waaMain',e=1,af=[('btn_waaASS','left',5),('btn_waaASS','right',5)],ac=('btn_waaASS','top',3,'sprt3'))
+  cmds.formLayout('form_waaMain',e=1,af=[('btn_waaAWH','left',5),('btn_waaAWH','right',5)],ac=('btn_waaAWH','top',3,'btn_waaASS'))
+  cmds.formLayout('form_waaMain',e=1,af=[('sprt4','left',5),('sprt4','right',5)],ac=('sprt4','top',5,'btn_waaAWH'))
+  
+  cmds.window('win_warAttr',e=1,widthHeight=[200,30],resizeToFitChildren=1)
+  cmds.showWindow('win_warAttr')
+
+ def addTag(self,txt,*a):
+  if(txt=='inputBelow'): txt = cmds.textField('txt_waaATC',q=1,text=1)
+
+  for x in cmds.ls(selection=1):
+   if cmds.objExists(x+'.warTag') == 0 :
+    cmds.addAttr(x,longName='warTag',dataType='string')
+    cmds.setAttr(x+'.warTag',txt,type='string')
+   else :
+    cmds.warning(x+' warTag alreay exist.')
+
+ def addPosXY(self,*a):
+  for x in cmds.ls(selection=1) :
+   if cmds.objExists(x+'.warPosX') == 0 :
+    cmds.addAttr(x,longName='warPosX',attributeType='long')
+   if cmds.objExists(x+'.warPosY') == 0 :
+    cmds.addAttr(x,longName='warPosY',attributeType='long')
+   
+ def addAttachObjs(self,*a):
+  sl = cmds.ls(selection=1)
+  eList = ''
+  for i,x in enumerate(sl[:-1]):
+   eList = eList + x + ':'
+  print(eList)
+  if eList != '' :
+   if cmds.objExists(sl[-1]+'.warAttach') == 0 :
+    cmds.addAttr(sl[-1],longName='warAttach',attributeType='enum',enumName=eList)
+   else : cmds.warning(x+'.warAttach is already exist.')
+  else : cmds.warning('Select multi objects.')
+    
+ def addAttachSide(self,*a):
+  for x in cmds.ls(selection=1) :
+   if cmds.objExists(x+'.warAttachSide') == 0 :
+    cmds.addAttr(x,longName='warAttachSide',attributeType='enum',enumName='top:bottom:left:right:')
+
+ def addShape(self,*a):
+  for x in cmds.ls(selection=1) :
+   if cmds.objExists(x+'.warShape') == 0 :
+    cmds.addAttr(x,longName='warShape',attributeType='enum',enumName='rectangle:circle:triangleU:triangleB:triangleL:triangleR:diamond:trapezoidU:trapezoidD:hexagon:')
+    
+ def addWidthHeight(self,*a):
+  for x in cmds.ls(selection=1) :
+   if cmds.objExists(x+'.warWidth') == 0 :
+    cmds.addAttr(x,longName='warWidth',attributeType='long')
+    cmds.setAttr(x+'.warWidth',10)
+   if cmds.objExists(x+'.warHeight') == 0 :
+    cmds.addAttr(x,longName='warHeight',attributeType='long')
+    cmds.setAttr(x+'.warHeight',10)
+
+as_warAttr()
